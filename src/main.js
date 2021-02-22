06e35e26ea4c51ce6edb3f70df7f484ad2fe29fe
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-22 16:14:04 +0530

    added bootsrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
 new Vue({
   el: '#app',
   render: h => h(App)
